c423f5769f5bef6153238185046f38af35be3101
albertkemp <albertkemp@5120L-144570-M.local>
2024-07-17 11:18:42 +0200

    Added link to github profile

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
 document.getElementById('icona').addEventListener("click", function(){
     window.location.href='index.html';
 });
+document.getElementById("githubicon").addEventListener("click", function(){
+    window.open('https://github.com/albertkemp', '_blank');
+});
